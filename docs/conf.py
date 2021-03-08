e92543ab478f57816eb2a0b0e3f8b3ce63e81d23
banteg <banteeg@gmail.com>
2021-03-08 14:31:23 +0300

    chore: bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, banteg'
 author = 'banteg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
