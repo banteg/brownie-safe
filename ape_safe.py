9b7f9fd7475c8ccb0f433d208f7561b8e526ee22
OwlOfMoistness <owl@yieldguild.games>
2021-12-13 16:47:02 +0100

    Add execute_transaction_with_framce

--- ape_safe.py
+++ ape_safe.py
@@ -324,6 +324,25 @@ class ApeSafe(Safe):
         receipt = signer.transfer(payload['to'], payload['value'], gas_limit=payload['gas'], data=payload['data'])
         return receipt
 
+    def execute_transaction_with_frame(self, safe_tx: SafeTx, frame_rpc="http://127.0.0.1:1248") -> bytes:
+        """
+        Execute a fully signed transaction with frame. Use this option with hardware wallets.
+        """
+        # Requesting accounts triggers a connection prompt
+        frame = Web3(Web3.HTTPProvider(frame_rpc, {'timeout': 600}))
+        account = frame.eth.accounts[0]
+        executor = accounts.at(account, force=True)
+        payload = safe_tx.w3_tx.buildTransaction()
+        tx = {
+            "from": account,
+            "to": self.address,
+            "value": safe_tx.value,
+            "nonce": executor.nonce,
+            "gas": web3.toHex(payload["gas"]),
+            "data": HexBytes(safe_tx.data),
+        }
+        frame.eth.send_transaction(tx)
+
     def preview_pending(self, events=True, call_trace=False):
         """
         Dry run all pending transactions in a forked environment.
