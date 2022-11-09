1986b3b5e0618ce704e45dee8a2ee8fab895ae35
ib-tycho <98139792+ib-tycho@users.noreply.github.com>
2022-11-10 03:41:06 +0800

    fix: update transaction service endpoints (#44)

--- ape_safe.py
+++ ape_safe.py
@@ -34,15 +34,15 @@ transaction_service = {
     1: 'https://safe-transaction.mainnet.gnosis.io',
     4: 'https://safe-transaction.rinkeby.gnosis.io',
     5: 'https://safe-transaction.goerli.gnosis.io',
-    10: 'https://safe-transaction.optimism.gnosis.io',
+    10: 'https://safe-transaction-optimism.safe.global',
     56: 'https://safe-transaction.bsc.gnosis.io',
     100: 'https://safe-transaction.xdai.gnosis.io',
     137: 'https://safe-transaction.polygon.gnosis.io',
     250: 'https://safe-txservice.fantom.network',
     246: 'https://safe-transaction.ewc.gnosis.io',
     288: 'https://safe-transaction.mainnet.boba.network',
-    42161: 'https://safe-transaction.arbitrum.gnosis.io',
-    43114: 'https://safe-transaction.avalanche.gnosis.io',
+    42161: 'https://safe-transaction-arbitrum.safe.global',
+    43114: 'https://safe-transaction-avalanche.safe.global',
     73799: 'https://safe-transaction.volta.gnosis.io',
 }
 
