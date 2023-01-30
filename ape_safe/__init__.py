34699da70c120c2e8736f7e140f70691a773e463
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2023-01-30 18:49:38 -0500

    feat: add MultiSend support

--- ape_safe/__init__.py
+++ ape_safe/__init__.py
@@ -1,8 +1,15 @@
 from ape import plugins
 
 from .accounts import AccountContainer, SafeAccount
+from .multisend import MultiSend
 
 
 @plugins.register(plugins.AccountPlugin)
 def account_types():
     return AccountContainer, SafeAccount
+
+
+__all__ = [
+    "MultiSend",
+    "SafeAccount",
+]
