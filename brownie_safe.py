580e59b3a0421c0ce164408aea9c3c19641f538d
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-05-29 13:53:13 +0200

    fix: retrieve abi via contract wrapper

--- brownie_safe.py
+++ brownie_safe.py
@@ -340,7 +340,7 @@ class BrownieSafe:
 
     def preview_tx(self, safe_tx: SafeTx, events=True, call_trace=False) -> TransactionReceipt:
         tx = copy(safe_tx)
-        safe = Contract.from_abi('Gnosis Safe', self.address, self.get_contract().abi)
+        safe = Contract.from_abi('Gnosis Safe', self.address, self.contract.abi)
         # Replace pending nonce with the subsequent nonce, this could change the safe_tx_hash
         tx.safe_nonce = safe.nonce()
         # Forge signatures from the needed amount of owners, skip the one which submits the tx
